8dda25f04d4f4c549ccd28207375c525c189402c
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-06 00:20:21 +0530

    File server added

--- cmd/webApp/routes.go
+++ cmd/webApp/routes.go
@@ -18,5 +18,17 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+	// Creates a fileserver by telling it where the static directory exists
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	// registers a new route in the mux router that matches requests for URLs starting with "/static/"
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
+	// The use of http.StripPrefix
+	/*
+		When a user requests a static file, they typically use a URL that looks something like this: http://example.com/static/myfile.css. However, the http.FileServer handler expects to be given a URL that contains only the path to the file relative to the directory it is serving, without any extra prefixes. In this case, it would expect a URL like /myfile.css.
+
+		To remove the "/static/" prefix from the requested URL, we use the http.StripPrefix handler. This handler modifies the incoming request by removing the "/static/" prefix from the requested URL before passing it on to the http.FileServer handler. This way, the http.FileServer handler receives a URL that contains only the path to the file relative to the "static" directory, and it can serve the correct file.
+	*/
 	return mux
 }
