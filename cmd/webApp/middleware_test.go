c1f85187ab248cc3ea81f808aab83784496ce8aa
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-11 22:19:01 +0530

    Added: Test for middleware.go file and also created a setup_test.go file to perform some logic before testing

--- cmd/webApp/middleware_test.go
+++ cmd/webApp/middleware_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestWriteToConsole(t *testing.T) {
+	var myH myHandler
+
+	h := WriteToConsole(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Errorf("does not return a http.Handler, returns a %T", v)
+	}
+}
+
+func TestSessionLoad(t *testing.T) {
+	var myH myHandler
+
+	h := SessionLoad(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Errorf("does not return a http.Handler, returns a %T", v)
+	}
+}
+
+func TestNoSurf(t *testing.T) {
+	var myH myHandler
+
+	h := Nosurf(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Errorf("does not return a http.Handler, returns a %T", v)
+	}
+}
