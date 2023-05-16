273b35d91ad18592cf58a1fc3ffb4cf497fc34f0
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-16 13:43:18 +0530

    Created: Database function for inserting a new reservation - InsertReservation()

---
+++ internal/repository/dbrepo/postgres.go
@@ -0,0 +1,28 @@
+package dbrepo
+
+import (
+	"context"
+	"time"
+
+	"github.com/ASHWIN776/learning-Go/internal/models"
+)
+
+// To insert the form data from make-reservation form to the reservations table in the database
+func (p *postgresDBRepo) InsertReservation(res models.Reservation) error {
+	ctx, cancel := context.WithTimeout(context.Background())
+
+	defer cancel()
+
+	stmt := `insert into reservations 
+	(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) 
+	values 
+	($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := p.DB.ExecContext(ctx, stmt, res.FirstName, res.LastName, res.Email, res.Phone, res.StartDate, res.EndDate, res.RoomID, time.Now(), time.Now())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
