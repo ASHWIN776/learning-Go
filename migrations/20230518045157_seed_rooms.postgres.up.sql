111f9f7debe40299dae7816c88b29cd93ee5ef73
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-18 10:23:05 +0530

    Added Migration: Seed rooms

---
+++ migrations/20230518045157_seed_rooms.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals-Quarters','2023-05-16 00:00:00.000','2023-05-16 00:00:00.000'),
+	 ('Majors-suite','2023-05-17 00:00:00.000','2023-05-17 00:00:00.000');
