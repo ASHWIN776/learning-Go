4c1f72b771795a84cc9aee920fc4998aaee40c2a
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-18 10:25:08 +0530

    Added Migration: Seed Restrictions

---
+++ migrations/20230518045347_seed_restrictions.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Booking','2023-05-15 00:00:00.000','2023-05-15 00:00:00.000'),
+	 ('Owner Block','2023-05-15 00:00:00.000','2023-05-15 00:00:00.000');
