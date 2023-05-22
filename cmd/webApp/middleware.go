cffccd6f7523f9dbde432807ff80f5a0c76c397d
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-22 17:26:29 +0530

    Added Auth Middleware: checks for the user auth status

--- cmd/webApp/middleware.go
+++ cmd/webApp/middleware.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ASHWIN776/learning-Go/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -29,4 +30,19 @@ func Nosurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+func Auth(next http.Handler) http.Handler {
+	// The below HandlerFunc() will give the passed in function access to the response writer and the request
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			// Put an error alert msg in the session and redirect to the login screen
+			session.Put(r.Context(), "error", "user not logged in")
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+
+		// for running the next middleware or the handler itself
+		next.ServeHTTP(w, r)
+	})
 }
